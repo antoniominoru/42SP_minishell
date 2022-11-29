7005b75f140e3f8123b84f1b565fa57f9dee2ff1
joaohuman <joaovictorgrodrigues@gmail.com>
2022-11-28 22:16:46 -0300

    update

--- src/minishell.c
+++ src/minishell.c
@@ -6,23 +6,25 @@
 /*   By: jvictor- <jvictor-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/24 21:41:53 by jvictor-          #+#    #+#             */
-/*   Updated: 2022/11/28 21:38:38 by jvictor-         ###   ########.fr       */
+/*   Updated: 2022/11/28 22:12:35 by jvictor-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int	main(void)
+int	main(int argc, char *argv[]) //char *envp[])
 {
+	if (argc > 1 && argv)
+		return (printf("erro ao executar, correto é: ./minishell\n"), 1);
 	printf("Funcionou!\n");
  	char *string;
-     char ex;
+    char ex;
 
-     ex = 'a';
-     string = &ex;
- 	while(*string != 'b')
+    ex = 'a';
+    string = &ex;
+ 	while(*string != 'q')
  	{
- 		string = readline(">>>>>>>>>");
+ 		string = readline("MiniHELL>>");
          printf("%s\n", string);
  	}
      free(string);
