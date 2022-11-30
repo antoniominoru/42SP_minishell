3a5a8e45c99b5978a4c8b2a87c78e7f43ffecdc4
aminoru- <aminoru-@student.42sp.org.br>
2022-11-30 23:27:46 +0100

    norminette ok

--- include/minishell.h
+++ include/minishell.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jvictor- <jvictor-@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: aminoru- <aminoru-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/20 19:56:27 by aminoru-          #+#    #+#             */
-/*   Updated: 2022/11/28 21:11:33 by jvictor-         ###   ########.fr       */
+/*   Updated: 2022/11/30 23:22:12 by aminoru-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,9 @@
 # include "../lib/libft/get_next_line.h"
 
 // lib of readline
-#include <stdio.h>
-#include <readline/readline.h>
-#include <readline/history.h>
-#include <stdlib.h>
+# include <stdio.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+# include <stdlib.h>
 
 #endif
--- src/minishell.c
+++ src/minishell.c
@@ -6,23 +6,23 @@
 /*   By: aminoru- <aminoru-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/24 21:41:53 by jvictor-          #+#    #+#             */
-/*   Updated: 2022/11/30 23:16:10 by aminoru-         ###   ########.fr       */
+/*   Updated: 2022/11/30 23:26:45 by aminoru-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int	main(int argc, char *argv[]) //char *envp[])
+int	main(int argc, char *argv[], char *envp[])
 {
- 	char *string;
+	char	*string;
 
-    string = NULL;
-	if (argc > 1 && argv)
+	string = NULL;
+	if (argc > 1 && argv && envp)
 		return (printf("erro ao executar, correto é: ./minishell\n"), 1);
- 	while(1)
- 	{
- 		string = readline("MiniHELL>>");
-        printf("%s\n", string);
- 	}
-    free(string);
+	while (1)
+	{
+		string = readline("MiniHELL>>");
+		printf("%s\n", string);
+	}
+	free(string);
 }
