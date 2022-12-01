871ebfd112e8464824ffba62c12d236fa80555f2
aminoru- <aminoru-@student.42sp.org.br>
2022-12-01 23:01:51 +0100

    norminete in history.c

--- src/history.c
+++ src/history.c
@@ -3,33 +3,32 @@
 /*                                                        :::      ::::::::   */
 /*   history.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jvictor- <jvictor-@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: aminoru- <aminoru-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/30 21:04:00 by jvictor-          #+#    #+#             */
-/*   Updated: 2022/11/30 22:05:26 by jvictor-         ###   ########.fr       */
+/*   Updated: 2022/12/01 23:01:13 by aminoru-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void    print_list(t_list *lst)
+void	print_list(t_list *lst)
 {
-    while (lst->next)
-    {
-        printf("%s\n", lst->content);
-        lst = lst->next;
-    }
-    printf("%s\n", lst->content);
+	while (lst->next)
+	{
+		printf("%s\n", lst->content);
+		lst = lst->next;
+	}
+	printf("%s\n", lst->content);
 }
 
 int	ft_add_history(char *cmd)
 {
-    static t_list *history;
+	static t_list	*history;
 
-    if (history == NULL)
-        history = ft_lstnew(cmd);
-    else
-        ft_lstadd_back(&history, ft_lstnew(cmd));
-    //print_list(history);
-    return (0);
+	if (history == NULL)
+		history = ft_lstnew(cmd);
+	else
+		ft_lstadd_back(&history, ft_lstnew(cmd));
+	return (0);
 }
