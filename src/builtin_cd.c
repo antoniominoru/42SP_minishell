74f359d4dbb598af2e2b8cd8ea66f50a0390aaf3
aminoru- <aminoru-@student.42sp.org.br>
2022-12-20 02:00:20 +0100

    update

--- src/builtin_cd.c
+++ src/builtin_cd.c
@@ -1,18 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   builtin_export.c                                   :+:      :+:    :+:   */
+/*   builtin_cd.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: aminoru- <aminoru-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/12/06 02:02:38 by aminoru-          #+#    #+#             */
-/*   Updated: 2022/12/20 01:42:08 by aminoru-         ###   ########.fr       */
+/*   Created: 2022/12/20 01:43:33 by aminoru-          #+#    #+#             */
+/*   Updated: 2022/12/20 01:44:26 by aminoru-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int	builtin_export(char *cmd, t_list *envp)
+int	builtin_cd(char *cmd, t_list *envp)
 {
 	ft_lstadd_back(&envp, ft_lstnew(cmd));
 	return (1);
--- test_minunit/test.c
+++ test_minunit/test.c
@@ -6,7 +6,7 @@
 /*   By: aminoru- <aminoru-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/05 14:53:44 by aminoru-          #+#    #+#             */
-/*   Updated: 2022/12/20 01:30:56 by aminoru-         ###   ########.fr       */
+/*   Updated: 2022/12/20 01:34:06 by aminoru-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -63,10 +63,10 @@ static char	*test_builtin_unset_finish(void)
 
 static char	*test_builtin_export(void)
 {
-	t_list *lst1 = ft_lstnew("First");
-	builtin_export("Second", lst1);
-	mu_assert("ERROR: builtin_export()", !strcmp(lst1->content, "First"));
-	mu_assert("ERROR: builtin_export()", !strcmp(lst1->next->content, "Second"));
+	t_list *lst1 = ft_lstnew("TESTE=teste");
+	builtin_export("FAST=fast", lst1);
+	mu_assert("ERROR: builtin_export()", !strcmp(lst1->content, "TESTE=teste"));
+	mu_assert("ERROR: builtin_export()", !strcmp(lst1->next->content, "FAST=fast"));
 	return (0);
 }
 
