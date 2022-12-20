9379b75b08095c12975e09e2a22bfe15d6b77b47
aminoru- <aminoru-@student.42sp.org.br>
2022-12-21 00:24:46 +0100

    refactor and separe test_builtin_exit.c

--- test_minunit/test.c
+++ test_minunit/test.c
@@ -6,21 +6,13 @@
 /*   By: aminoru- <aminoru-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/05 14:53:44 by aminoru-          #+#    #+#             */
-/*   Updated: 2022/12/20 23:45:41 by aminoru-         ###   ########.fr       */
+/*   Updated: 2022/12/21 00:04:30 by aminoru-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minunit.h"
 
 int tests_run = 0;
-
-static char	*test_builtin_exit(void)
-{
-	int result = builtin_exit();
-	
-	mu_assert("ERROR: builtin_exit()", (result == 0));
-	return (0);
-}
 
 static char	*test_builtin_env(void)
 {
---
+++ test_minunit/test_builtin_exit.c
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   test_builtin_exit.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aminoru- <aminoru-@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/20 23:49:47 by aminoru-          #+#    #+#             */
+/*   Updated: 2022/12/21 00:07:00 by aminoru-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minunit.h"
+
+char	*test_builtin_exit(void)
+{
+	int result = builtin_exit();
+	
+	mu_assert("ERROR: builtin_exit()", (result == 0));
+	return (0);
+}
