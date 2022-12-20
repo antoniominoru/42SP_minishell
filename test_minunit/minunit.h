9c17645dcdf3724a3d883c075fc59ec9e16b3421
aminoru- <aminoru-@student.42sp.org.br>
2022-12-21 00:26:52 +0100

    refactor and separe test_builtin_env.c

--- test_minunit/minunit.h
+++ test_minunit/minunit.h
@@ -6,7 +6,7 @@
 /*   By: aminoru- <aminoru-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/05 15:18:23 by aminoru-          #+#    #+#             */
-/*   Updated: 2022/12/21 00:05:07 by aminoru-         ###   ########.fr       */
+/*   Updated: 2022/12/21 00:25:33 by aminoru-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,6 +27,7 @@ char	*test_builtin_unset_initial(void);
 char	*test_builtin_unset_between(void);
 char	*test_builtin_unset_finish(void);
 char	*test_builtin_exit(void);
+char	*test_builtin_env(void);
 
 /* file: minunit.h */
  #define mu_assert(message, test) do { if (!(test)) return message; } while (0)
