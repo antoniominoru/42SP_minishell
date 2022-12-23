312ef236118984f763fb77d98d56c35d74345b33
aminoru- <aminoru-@student.42sp.org.br>
2022-12-23 01:12:04 +0100

    update

--- src/builtin_echo.c
+++ src/builtin_echo.c
@@ -6,7 +6,7 @@
 /*   By: aminoru- <aminoru-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/21 01:53:57 by jvictor-          #+#    #+#             */
-/*   Updated: 2022/12/23 00:33:13 by aminoru-         ###   ########.fr       */
+/*   Updated: 2022/12/23 01:11:10 by aminoru-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,8 @@
 
 void	builtin_echo(char *cmd)
 {
-	// if (ft_strnstr(cmd, "-n ", 7))
-	// 	printf("%s", cmd + 3);
-	// else
-	printf("%s\n", cmd);
+	if (ft_strnstr(cmd, "-n ", 7))
+		printf("%s", cmd + 3);
+	else
+		printf("%s\n", cmd);
 }
--- src/tokenizer.c
+++ src/tokenizer.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   tokenizer.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jvictor- <jvictor-@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: aminoru- <aminoru-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/23 00:05:00 by jvictor-          #+#    #+#             */
-/*   Updated: 2022/12/23 00:05:17 by jvictor-         ###   ########.fr       */
+/*   Updated: 2022/12/23 01:11:25 by aminoru-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 char	**tokenizer(char *cmd)
 {
 	char	**cmd_tkn;
-	
+
 	cmd_tkn = ft_split(cmd, ' ');
 	return (cmd_tkn);
 }
