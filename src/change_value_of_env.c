7f81bfe253b936586a4eba1a021256ee7d090df9
aminoru- <aminoru-@student.42sp.org.br>
2023-01-17 00:53:55 +0100

    fix bug change_value_of_env

--- src/change_value_of_env.c
+++ src/change_value_of_env.c
@@ -6,7 +6,7 @@
 /*   By: aminoru- <aminoru-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/10 01:41:41 by aminoru-          #+#    #+#             */
-/*   Updated: 2023/01/14 01:33:44 by aminoru-         ###   ########.fr       */
+/*   Updated: 2023/01/17 00:06:53 by aminoru-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,21 +14,6 @@
 
 int	change_value_of_env(char *cmd, char *val, t_list **envp)
 {
-	t_list	*tmp;
-	char	*value;
-
-	tmp = *envp;
-	value = ft_strjoin(cmd, ft_strjoin("=", val));
-	if (ft_strncmp(cmd, tmp->content, ft_strlen(cmd)) == 0)
-		tmp->content = value;
-	while (tmp->next)
-	{
-		if (ft_strncmp(cmd, tmp->content, ft_strlen(cmd)) == 0)
-		{
-			tmp->content = value;
-			return (1);
-		}
-		tmp = tmp->next;
-	}
-	return (0);
+	builtin_export(ft_strjoin(cmd, ft_strjoin("=", val)), envp);
+	return (1);
 }
--- test_minunit/test_change_value_of_env.c
+++ test_minunit/test_change_value_of_env.c
@@ -6,7 +6,7 @@
 /*   By: aminoru- <aminoru-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/22 23:31:38 by aminoru-          #+#    #+#             */
-/*   Updated: 2023/01/10 01:30:57 by aminoru-         ###   ########.fr       */
+/*   Updated: 2023/01/17 00:22:33 by aminoru-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,15 @@
 // # Arrage
 char	*test_change_value_of_env(void)
 {
-	t_list	*lst1 = ft_lstnew("HOME=teste");
-	char	*test;
-	char	*espec = "mudou";
+	t_list *lst1 = NULL;
+
+	ft_lstadd_back(&lst1, ft_lstnew("HOME=First"));
+	ft_lstadd_back(&lst1, ft_lstnew("Second=Second2"));
 	
 	// # Act
-	change_value_of_env("HOME", "mudou", &lst1);
-	test = take_value_of_env("HOME", &lst1);
+	change_value_of_env("HOME", "mudoud", &lst1);
 	// # Assert
-	mu_assert("ERROR: test_change_value_of_env()", !strcmp(test, espec));
+	mu_assert("ERROR: test_change_value_of_env()", !strcmp(lst1->next->content, "HOME=mudoud"));
 	free(lst1);	
 	return (0);
 }
