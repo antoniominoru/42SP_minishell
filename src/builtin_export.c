2d6e72a3d08c1cf0db7e6a741e55dd5f063613e9
joaohuman <joaovictorgrodrigues@gmail.com>
2023-02-04 01:23:49 +0100

    resolvido leak builtin_export

--- src/builtin_export.c
+++ src/builtin_export.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   builtin_export.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aminoru- <aminoru-@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: jvictor- <jvictor-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/06 02:02:38 by aminoru-          #+#    #+#             */
-/*   Updated: 2023/01/31 00:18:50 by aminoru-         ###   ########.fr       */
+/*   Updated: 2023/02/04 01:21:49 by jvictor-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,16 @@ void	builtin_export(char *cmd, t_list **envp)
 {
 	char	**cmd_args;
 	char	*tmp;
+	char	*tmp2;
 
 	tmp = NULL;
+	tmp2 = NULL;
 	cmd_args = ft_split(cmd, '=');
 	builtin_unset(cmd_args[0], envp);
-	tmp = ft_strjoin(cmd_args[0], ft_strjoin("=", cmd_args[1]));
-	ft_lstadd_back(envp, ft_lstnew(tmp));
+	tmp = ft_strjoin("=", cmd_args[1]);
+	tmp2 = ft_strjoin(cmd_args[0], tmp);
+	ft_lstadd_back(envp, ft_lstnew(tmp2));
+	free_tkn(cmd_args);
+	free(tmp);
+	free(tmp2);
 }
