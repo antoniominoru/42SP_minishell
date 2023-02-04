d123076522020ab4f4c20b0530f9def40c5d0cfb
joaohuman <joaovictorgrodrigues@gmail.com>
2023-02-04 21:50:22 +0100

    builtin_exit com cod de erro

--- src/builtin_exit.c
+++ src/builtin_exit.c
@@ -3,18 +3,53 @@
 /*                                                        :::      ::::::::   */
 /*   builtin_exit.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aminoru- <aminoru-@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: jvictor- <jvictor-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/30 21:04:00 by jvictor-          #+#    #+#             */
-/*   Updated: 2023/01/31 00:36:31 by aminoru-         ###   ########.fr       */
+/*   Updated: 2023/02/04 21:42:53 by jvictor-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+static	int	count_args(char **cmd_tkn)
+{
+	int i;
+
+	i = 0;
+	while (cmd_tkn[i])
+		i++;
+	return (i);
+}
+
+static int arg_not_num(char *cmd_tkn)
+{
+	while (*cmd_tkn)
+		if (!ft_isdigit(*cmd_tkn++))
+			return (1);
+	return (0);
+}
+
 void	builtin_exit(t_list **lst_env, char *cmd, char **cmd_tkn)
 {
+	int	argc;
+	int arg_exit;
+	
+	argc = count_args(cmd_tkn);
+	arg_exit = 0;
+	if (argc > 2)
+	{
+		printf("exit: error, too many args.\n");
+		return ;
+	}
+	if (arg_not_num(cmd_tkn[1]))
+	{
+		printf("exit: error, numeric argument required.\n");
+		return ;
+	}
+	if (cmd_tkn[1])
+		arg_exit = atoi(cmd_tkn[1]);
 	free_all(lst_env, cmd, cmd_tkn);
 	printf("exit\n");
-	exit(0);
+	exit(arg_exit);
 }
