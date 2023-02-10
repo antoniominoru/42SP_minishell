7de98e3973389c2bcf5dda9756495c5af0b1c700
aminoru- <aminoru-@student.42sp.org.br>
2023-02-10 15:18:15 +0100

    include export without =

--- src/builtin_export.c
+++ src/builtin_export.c
@@ -6,7 +6,7 @@
 /*   By: aminoru- <aminoru-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/06 02:02:38 by aminoru-          #+#    #+#             */
-/*   Updated: 2023/02/10 01:48:28 by aminoru-         ###   ########.fr       */
+/*   Updated: 2023/02/10 15:16:37 by aminoru-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,15 +23,22 @@ void	builtin_export(char *cmd, t_list **envp)
 	if (cmd)
 	{
 		cmd_args = ft_split(cmd, '=');
-		builtin_unset(cmd_args[0], envp);
-		tmp = ft_strjoin("=", cmd_args[1]);
-		ft_lstadd_back(envp, ft_lstnew(ft_strjoin(cmd_args[0], tmp)));
+		if (count_vector(cmd_args) > 1)
+		{
+			builtin_unset(cmd_args[0], envp);
+			tmp = ft_strjoin("=", cmd_args[1]);
+			ft_lstadd_back(envp, ft_lstnew(ft_strjoin(cmd_args[0], tmp)));
+		}
+		else
+		{
+			builtin_unset(cmd_args[0], envp);
+			ft_lstadd_back(envp, ft_lstnew(ft_strdup(cmd_args[0])));
+		}
 		free_tkn(cmd_args);
 		free(tmp);
 	}
 	else
 		print_env(*envp);
-		
 }
 
 void	print_env(t_list *lst)
