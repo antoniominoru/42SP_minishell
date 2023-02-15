50d823643e44f89406a291f71f229c701d643a05
aminoru- <aminoru-@student.42sp.org.br>
2023-02-15 02:00:13 +0100

    update merge

--- src/quotes.c
+++ src/quotes.c
@@ -3,26 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   quotes.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jvictor- <jvictor-@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: aminoru- <aminoru-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/12 22:44:11 by jvictor-          #+#    #+#             */
-/*   Updated: 2023/02/14 17:00:49 by jvictor-         ###   ########.fr       */
+/*   Updated: 2023/02/15 01:58:00 by aminoru-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
-
-void	position_quotes(int pos, char q)
-{
-	if (!pos_quo_sin_2 && q == '\'')
-		pos_quo_sin_2 = pos;
-	else if (!pos_quo_sin_1 && q == '\'')
-		pos_quo_sin_1 = pos;
-	else if (!pos_quo_dbl_2 && q == '\"')
-		pos_quo_dbl_2 = pos;
-	else if (!pos_quo_dbl_1 && q == '\"')
-		pos_quo_dbl_1 = pos;
-}
 
 //se tem menos de 2 aspas ret = 0
 //se tem 2 aspas ret = 1
@@ -39,15 +27,9 @@ int	have_two_quotes(char *cmd)
 	while (len)
 	{
 		if (cmd[len] == '\'')
-		{
-			position_quotes(len, '\'');
 			how_many_single++;
-		}
 		if (cmd[len] == '\"')
-		{
-			position_quotes(len, '\"');
 			how_many_double++;
-		}
 		len--;
 	}
 	if (how_many_single == 2
@@ -57,16 +39,4 @@ int	have_two_quotes(char *cmd)
 		|| how_many_double > 2)
 		return (2);
 	return (0);
-}
-
-void	remove_quotes(char **cmd)
-{
-	char	*temp;
-
-	temp = ft_strtrim(cmd[pos_quo_sin_1], "\'");
-	//trim n da certo nessa situacao de agr pq ele tira tudo do comeco e do fim
-	free(*cmd);
-	*cmd = temp;
-	// preciso saber onde elas estao
-	// se elas sao iguais
 }
