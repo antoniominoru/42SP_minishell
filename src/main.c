0a0e2673b26fa761e2c95ed91628acc6169002f2
aminoru- <aminoru-@student.42sp.org.br>
2023-02-18 02:38:04 +0100

    update

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: aminoru- <aminoru-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/03 21:23:46 by jvictor-          #+#    #+#             */
-/*   Updated: 2023/02/16 17:38:03 by aminoru-         ###   ########.fr       */
+/*   Updated: 2023/02/18 02:37:08 by aminoru-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,12 +24,12 @@ int	main(int argc, char *argv[], char *envp[])
 	path = NULL;
 	status_error(NULL, NO_ERROR);
 	lst_env = env_to_lst(envp);
+	if (lst_env == NULL)
+		printf("erro");
 	set_path(&lst_env);
 	if (argc > 1 && argv && envp)
 		return (status_error("too many arguments. correct is: ./minishell",
 				ERROR), ERROR_ONE);
-	if (lst_env == NULL)
-		printf("erro"); //essa verificação esta correta aqui?
 	minishell(cmd, lst_env, &path);
 	free_part(&lst_env, &cmd, &path);
 }
