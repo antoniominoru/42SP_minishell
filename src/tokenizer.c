67553d84ce742b59f5b0fb8a6b780afa4793e907
aminoru- <aminoru-@student.42sp.org.br>
2023-02-18 23:35:13 +0100

    update

--- src/tokenizer.c
+++ src/tokenizer.c
@@ -6,7 +6,7 @@
 /*   By: aminoru- <aminoru-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/23 00:05:00 by jvictor-          #+#    #+#             */
-/*   Updated: 2023/02/18 20:18:32 by aminoru-         ###   ########.fr       */
+/*   Updated: 2023/02/18 21:40:54 by aminoru-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -98,7 +98,7 @@ char	**tokenizer(char *cmd, char **cmd_tkn, t_list **envp)
 	else
 		cmd_env_new = ft_strdup(cmd);
 	cmd_tkn_new = split_space_quotes(split_quotes(cmd_env_new, q), q);
-	cmd_tkn_new = remove_quotes(cmd_tkn_new, q);
 	cmd_tkn_new = remove_quotes(cmd_tkn_new, ';');
+	cmd_tkn_new = remove_quotes(cmd_tkn_new, q);
 	return (cmd_tkn_new);
 }
