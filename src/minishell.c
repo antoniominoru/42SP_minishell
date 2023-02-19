5e74856514b890962713e06ec1a6433b8720636f
aminoru- <aminoru-@student.42sp.org.br>
2023-02-19 20:54:45 +0100

    fix cat |

--- src/minishell.c
+++ src/minishell.c
@@ -6,7 +6,7 @@
 /*   By: aminoru- <aminoru-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/24 21:41:53 by jvictor-          #+#    #+#             */
-/*   Updated: 2023/02/19 18:54:19 by aminoru-         ###   ########.fr       */
+/*   Updated: 2023/02/19 20:54:10 by aminoru-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,17 +33,35 @@ static void	pipe_to_all(char *tmp, char *tmp2, t_list **lst_env)
 	builtin_pipe_to_all(tmp, lst_env);
 }
 
+static int	vec_pipe(char	**cmd)
+{
+	int		i;
+	char	**tmp;
+
+	i = 0;
+	tmp = cmd;
+	while (tmp[i + 1])
+		i++;
+	if (!ft_strncmp(tmp[i], "|", 1) && tmp[i + 1] == NULL)
+		return (1);
+	return (0);
+}
+
 static void	make_pipe(char *cmd, t_list **lst_env)
 {
 	char	**test;
 	char	*tmp;
 	char	*tmp2;
+	int		flag;
 
+	flag = 0;
 	test = NULL;
 	test = tokenizer(cmd, test, lst_env, 1);
 	tmp = test[0];
 	tmp2 = test[count_vector(test) - 1];
-	if (!ft_strncmp(tmp, "cat", 3) \
+	if (vec_pipe(test))
+		status_error("incomplete function", ERROR);
+	else if (!ft_strncmp(tmp, "cat", 3) \
 	&& !ft_strncmp(tmp2, "ls", 2) && cec(test))
 		pipe_to_all(tmp, tmp2, lst_env);
 	else
