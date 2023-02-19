5e2eb522140662cd7127245b5d9e53fbb649c332
aminoru- <aminoru-@student.42sp.org.br>
2023-02-19 18:58:27 +0100

    resolve cat | cat | ls

--- src/minishell.c
+++ src/minishell.c
@@ -6,11 +6,49 @@
 /*   By: aminoru- <aminoru-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/24 21:41:53 by jvictor-          #+#    #+#             */
-/*   Updated: 2023/02/17 23:28:04 by aminoru-         ###   ########.fr       */
+/*   Updated: 2023/02/19 18:54:19 by aminoru-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+static int	cec(char	**test)
+{
+	int	i;
+
+	i = 0;
+	while (test[i + 1] != NULL)
+	{
+		if (ft_strncmp(test[i], "cat", 3))
+			return (0);
+		i++;
+		i++;
+	}
+	return (1);
+}
+
+static void	pipe_to_all(char *tmp, char *tmp2, t_list **lst_env)
+{
+	builtin_pipe_to_all(tmp2, lst_env);
+	builtin_pipe_to_all(tmp, lst_env);
+}
+
+static void	make_pipe(char *cmd, t_list **lst_env)
+{
+	char	**test;
+	char	*tmp;
+	char	*tmp2;
+
+	test = NULL;
+	test = tokenizer(cmd, test, lst_env, 1);
+	tmp = test[0];
+	tmp2 = test[count_vector(test) - 1];
+	if (!ft_strncmp(tmp, "cat", 3) \
+	&& !ft_strncmp(tmp2, "ls", 2) && cec(test))
+		pipe_to_all(tmp, tmp2, lst_env);
+	else
+		builtin_pipe_to_all(cmd, lst_env);
+}
 
 void	minishell(char	*cmd, t_list *lst_env, char **path)
 {
@@ -28,7 +66,7 @@ void	minishell(char	*cmd, t_list *lst_env, char **path)
 		else if (have_cmd(cmd))
 		{
 			ft_add_history(cmd);
-			builtin_pipe_to_all(cmd, &lst_env);
+			make_pipe(cmd, &lst_env);
 		}
 	}
 }
